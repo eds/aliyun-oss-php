5d89eaab679b21f66ba2957aa7980076be805747
eds <ideayeah@gmail.com>
2015-07-27 18:30:18 +0800

    Update conf.inc.php

--- conf.inc.php
+++ conf.inc.php
@@ -8,9 +8,9 @@ defined('ALI_LOG') || define('ALI_LOG', false);
 
 // 日志路径，默认使用oss目录下的./logs/
 defined('ALI_LOG_PATH') || define('ALI_LOG_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR);
-if (!is_dir(ALI_LOG_PATH)){
-  mkdir(ALI_LOG_PATH, 0755, true);
-}
+// if (!is_dir(ALI_LOG_PATH)){
+//   mkdir(ALI_LOG_PATH, 0755, true);
+// }
 
 // 是否显示LOG输出
 defined('ALI_DISPLAY_LOG') || define('ALI_DISPLAY_LOG', false);
